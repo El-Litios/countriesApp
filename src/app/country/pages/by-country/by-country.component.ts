ea059430e74505ab3e414c42e5248b31128d0930
El-Litios <carlosguzmanrojas00@gmail.com>
2023-04-02 16:14:17 -0400

    Importacion de servicio y consumo

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-country',
   templateUrl: './by-country.component.html',
-  styleUrls: ['./by-country.component.css']
+  styleUrls: ['./by-country.component.css'],
 })
 export class ByCountryComponent {
+  searchText: string = '';
 
+  constructor(private serviceCountry: CountryService) {}
+
+  search() {
+    this.serviceCountry.search(this.searchText)
+    .subscribe(res => {
+      console.log(res);
+    });
+  }
 }
