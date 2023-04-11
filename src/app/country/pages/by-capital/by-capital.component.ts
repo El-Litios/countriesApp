186845b0e1c99ef94327612524098754940e0866
El-Litios <carlosguzmanrojas00@gmail.com>
2023-04-10 20:58:26 -0400

    Busqueda por capital

--- src/app/country/pages/by-capital/by-capital.component.ts
+++ src/app/country/pages/by-capital/by-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./by-capital.component.css']
 })
 export class ByCapitalComponent {
+  searchText: string = '';
+  errorMessage: boolean = false;
+  countries: Country[] = [];
 
+  constructor(private serviceCountry: CountryService) {}
+
+  searchByCapital( arg:string) {
+    this.errorMessage = false;
+    this.searchText = arg;
+    this.serviceCountry.searchCapital(this.searchText)
+    .subscribe((res) => {
+      console.log(res);
+      this.countries = res;
+    }, (err) => {
+      this.errorMessage = true
+      this.countries = []
+    });
+  }
+
+  Suggests(term: string){
+    this.errorMessage = false;
+    
+  }
 }
