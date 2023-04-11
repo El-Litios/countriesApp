c0fe14e3b644ab9121df3515dddd1e58c33b325c
El-Litios <carlosguzmanrojas00@gmail.com>
2023-04-10 20:58:14 -0400

    Metodo Debounce para observar cambios en el input

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,17 +1,37 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.css']
 })
-export class SearchInputComponent {
+export class SearchInputComponent implements OnInit{
+  
   searchText: string = '';
 
   //emite hacia el padre
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
-  
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  deBouncer: Subject<string> = new Subject();
+
+  ngOnInit() {
+    this.deBouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe( v => {
+      this.onDebounce.emit(v);
+      
+    })
+  }
+
   search(){
-    this.onEnter.emit(this.searchText);
+    this.onEnter.emit(this.searchText); 
+  }
+
+  keyPressed(){
+    this.deBouncer.next(this.searchText);
   }
 }
